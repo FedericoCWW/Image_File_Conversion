d98923eff17ab1fbd95f1562d0b82594a2af8bb9
Federico <133919977+FedericoCWW@users.noreply.github.com>
2024-07-02 22:28:57 -0300

    Segunda version

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import tkinter as tk
-from tkinter import ttk, messagebox, filedialog, Label
+from tkinter import ttk, messagebox, filedialog, Label, font
 from PIL import Image, ImageTk
 
 def import_file():
     global img,file_path
-    fileTypes = [("Image files", "*.png;*.jpg;*.jpeg")]
+    fileTypes = [("Image files", "*.png;*.jpg;*.jpeg; *.webp")]
     file_path = tk.filedialog.askopenfilename(title="Select a file", filetypes=fileTypes)
     print(file_path)
+    label.config(text=f"Image loaded:{file_path}")
     if file_path:
-        #HACER ESTA PARTE CON PIL
         img = Image.open(file_path)
-        ##pic = ImageTk.PhotoImage(img)
 
 
 def convert():
@@ -20,20 +19,20 @@ def convert():
     if file_path[-3:] == outf:
         print("the fileformats are the same, choce another one") #hacer con un mgsbox
     else:
-        export_file_path = filedialog.asksaveasfilename(defaultextension='.jpg',
-        filetypes=[("Image files", f"{outf}")])
+        export_file_path = filedialog.asksaveasfilename(defaultextension='.jpg', filetypes=[("Image files", f"{outf}")])
         img.save(export_file_path)
         messagebox.showinfo(title="Completed Conversion" ,message="Succesfully Converted!")
 
 
-
 root = tk.Tk()
 root.title("Image File Converter")
-root.geometry("400x200")
-import_b = tk.Button(root, text="Import File", command=import_file)
+root.geometry("600x220")
+import_b = tk.Button(root, text="Import File", font="Arial 14",command=import_file)
 import_b.pack(pady=20)
+label = Label(text="")
+label.pack(pady=10)
 combobox = ttk.Combobox(root, values=[".jpg", ".png", ".tga", ".bmp", ".webp"])
 combobox.pack()
-convert_b = tk.Button(root, text="Convert", command=convert)
+convert_b = tk.Button(root, text="Convert", font="Arial 14",command=convert)
 convert_b.pack(pady=20)
 root.mainloop()
